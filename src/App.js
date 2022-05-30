d01fdde53c50f2ddcc9a1959cc568964eeee5780
Subramaniam Balasegaran <blsgrn@tutanota.com>
2022-05-30 07:43:46 +0530

    Add searchbox

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from "react";
 import Header from "./components/Header";
 import CityList from "./components/CityList";
 import cityArray from "./data/CityArray";
+import SearchBox from "./components/SearchBox";
+import { useState } from "react";
 
 function App() {
+  const [searchField, setSearchField] = useState("");
+
+  function handleChange(e) {
+    setSearchField(e.target.value);
+  }
+
+  const filteredCities = cityArray.filter((city) =>
+    city.toLowerCase().includes(searchField.toLowerCase())
+  );
+
   return (
     <>
       <Header text="Weather App" />
-      <CityList arr={cityArray} />
+      <SearchBox placeholder="Enter a city" handleChange={handleChange} />
+      <CityList arr={filteredCities} />
     </>
   );
 }
