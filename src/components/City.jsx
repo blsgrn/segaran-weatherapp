c207bcb4657977ddd76f64bfd9a922ade1339f72
Subramaniam Balasegaran <blsgrn@tutanota.com>
2022-06-02 15:08:55 +0530

    Add state to weather info retrieved from API

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PHV from "./PHV";
 import Colorized from "./Colorized";
 
 function City({ code }) {
   const [city, setCity] = useState();
-  const [country, setCountry] = useState();
+  const [nation, setCountry] = useState();
 
   const [cloud, setCloud] = useState();
-  const [description, setDescription] = useState();
+  const [desc, setDescription] = useState();
 
   const [temperature, setTemp] = useState();
   const [t_min, setTempMin] = useState();
@@ -22,59 +22,60 @@ function City({ code }) {
   const [sunriseT, setSunrise] = useState();
   const [sunsetT, setSunset] = useState();
 
-  const api = `bbcb8ad070b251ae782fc83a6754b820`;
+  const [weather, setWeather] = useState({});
 
-  const base = `https://api.opennnweathermap.org/data/2.5/weather?id=${code}&appid=${api}`;
+  useEffect(() => {
+    const api = `bbcb8ad070b251ae782fc83a6754b820`;
+    const base = `https://api.openweathermap.org/data/2.5/weather?id=${code}&appid=${api}`;
+    fetch(base)
+      .then((response) => response.json())
+      .then((result) => {
+        setWeather(result);
 
-  fetch(base)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const { name, visibility } = data;
-      const { sunrise, sunset, country } = data.sys;
-      const { description, icon } = data.weather[0];
-      const { temp, temp_min, temp_max, pressure, humidity } = data.main;
-      const { speed, deg } = data.wind;
+        // const { name, visibility } = result;
+        // const { sunrise, sunset, country } = result.sys;
+        // const { description, icon } = result.weather[0];
+        // const { temp, temp_min, temp_max, pressure, humidity } = result.main;
+        // const { speed, deg } = result.wind;
 
-      console.log(country, sunrise, sunset, description, icon);
-      console.log(temp, temp_min, temp_max, pressure, humidity, visibility);
-      console.log(speed, deg);
+        // console.log(country, sunrise, sunset, description, icon);
+        // console.log(temp, temp_min, temp_max, pressure, humidity, visibility);
+        // console.log(speed, deg);
+        // console.log(result);
 
-      const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-      const sunriseGMT = new Date(sunrise * 1000);
-      const sunsetGMT = new Date(sunset * 1000);
+        // const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+        // const sunriseGMT = new Date(sunrise * 1000);
+        // const sunsetGMT = new Date(sunset * 1000);
 
-      setCity(name);
-      setCountry(country);
-      setCloud(iconUrl);
-      setDescription(description);
+        // setCity(name);
+        // setCountry(country);
+        // setCloud(iconUrl);
+        // setDescription(description);
 
-      setTemp(temp);
-      setTempMin(temp_min);
-      setTempMax(temp_max);
+        // setTemp(temp);
+        // setTempMin(temp_min);
+        // setTempMax(temp_max);
 
-      setPressure(pressure);
-      setHumidity(humidity);
-      setVisibility(visibility);
-      setSpeed(speed);
-      setDeg(deg);
-      setSunrise(sunriseGMT);
-      setSunset(sunsetGMT);
-    });
+        // setPressure(pressure);
+        // setHumidity(humidity);
+        // setVisibility(visibility);
+        // setSpeed(speed);
+        // setDeg(deg);
+        // setSunrise(sunriseGMT);
+        // setSunset(sunsetGMT);
+      })
+      .catch((error) => console.log(error));
+  }, [code]);
 
-  // function handleClick() {
-  //   navigate(`/city`);
-  // }
-  // onClick={() => handleClick()}
+  console.log(weather);
 
   return (
     <div className="card">
       <Colorized
         city={city}
-        country={country}
+        country={nation}
         cloud={cloud}
-        description={description}
+        description={desc}
         temp={temperature}
         min={t_min}
         max={t_max}
